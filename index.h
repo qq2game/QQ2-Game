065be6a8d294b2285c94885a777a10f86760d164
qq2game <arman10turk@gmail.com>
2026-02-25 12:53:50 +0530

    Add files via upload

---
+++ index.h
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Game Download</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <style>
+        body{
+            font-family: Arial;
+            background:#f2f2f2;
+            text-align:center;
+        }
+        .card{
+            margin-top:40px;
+        }
+        img{
+            width:200px;
+            border-radius:20px;
+        }
+        .btn{
+            display:inline-block;
+            margin-top:20px;
+            padding:12px 25px;
+            background:darkred;
+            color:white;
+            text-decoration:none;
+            border-radius:25px;
+        }
+        table{
+            margin:auto;
+            margin-top:20px;
+            border-collapse:collapse;
+        }
+        td{
+            border:1px solid gray;
+            padding:10px 20px;
+        }
+    </style>
+</head>
+<body>
+
+<div class="card">
+    <h2>QQ2 Game APK</h2>
+    <img src="logo.png"><br>
+    <a href="#" class="btn">QQ2 Game Download </a>
+
+    <table>
+        <tr>
+            <td>Name</td>
+            <td>QQ2 Game APK</td>
+        </tr>
+        <tr>
+            <td>Size</td>
+            <td>29MB</td>
+        </tr>
+        <tr>
+            <td>Version</td>
+            <td>1.3.6v</td>
+        </tr>
+    </table>
+</div>
+
+</body>
+</html>
